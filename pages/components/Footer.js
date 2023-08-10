60ddedec937f4a050a09ee5501e8d80d032ce1c7
Sai Charan <saicharan20012007@gmail.com>
2023-08-10 18:05:32 +0530

    footer done

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,7 +1,45 @@
+import Link from "next/link";
+import {AiFillHome}  from 'react-icons/ai'
+import {FcAbout, FcAreaChart} from 'react-icons/fc'
+import {BsPhone} from 'react-icons/bs'
+import {GrServices} from 'react-icons/gr'
+import {BiLogoFacebookCircle} from 'react-icons/bi'
+import {BiLogoTwitter} from 'react-icons/bi'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {BsTelephoneOutboundFill} from 'react-icons/bs'
+import {IoIosMail} from 'react-icons/io'
+
 const Footer = () => {
     return (
-        <div className="container-fluid p-4 bg-light text-dark">
-        <h3>Footer Info</h3>
+
+        <div className="container-fluid p-4 bg-light text-dark footer-container">
+        <div className="logo-container">
+        <img src="https://res.cloudinary.com/dlovqnrza/image/upload/v1691648176/site-logo_vhtzvm.png" className = "footer-logo" alt="logo"/>
+
+        </div>
+        <div className="quick-links-container">
+        <h1 className="footer-titles">QUICK LINKS</h1>
+        <Link href="/"> <p className="footer-des"> <AiFillHome className="footer-icons"/> Home</p></Link>
+       <Link href="/about"> <p className="footer-des"> <FcAbout className="footer-icons"/>About Us</p></Link>
+        <Link href="/contact"> <p  className="footer-des"><BsPhone className="footer-icons"/>Contact Us</p></Link>
+        <Link href="/services" ><p className="footer-des"><GrServices className="footer-icons"/>Services Us</p></Link>
+        </div>
+        <div className="contact-us-links">
+        <h1 className="footer-titles">SOCIAL NETWORKS</h1>
+       <a href="https://www.facebook.com/groups/globalindiansinfo/" target="_blank"> <p className="footer-des"><BiLogoFacebookCircle/> Facebook</p></a>
+       <a href="https://twitter.com/globalindian_in" target="_blank"> <p className="footer-des"><BiLogoTwitter/> Twitter</p></a>
+       <a href="https://www.linkedin.com/in/global-indians-info-b87034259/?originalSubdomain=in" target="_blank"> <p className="footer-des"><AiFillLinkedin/> Linkedin</p></a>
+        <a href="https://www.instagram.com/global_indian_official/https://www.instagram.com/global_indian_official/" target="_blank"><p className="footer-des"><AiOutlineInstagram/> Instagram</p></a>
+
+        </div>
+        <div className="contact-us-links">
+        <h1 className="footer-titles">CONTACT US </h1>
+       <a tel="+44-7867090363" target="_blank"> <p className="footer-des"><BsTelephoneOutboundFill/> +44-7867090363</p></a>
+       <a href="info@prabisha.com" target="_blank"> <p className="footer-des"><IoIosMail/> info@prabisha.com</p></a>
+
+        
+        </div>
         </div>
     )
 }
