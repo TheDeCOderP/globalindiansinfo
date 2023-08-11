a55d9f64f822ab05c1f309b05f2fcb5b95ca1947
Sai Charan <saicharan20012007@gmail.com>
2023-08-11 14:32:03 +0530

    footer updated

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -9,21 +9,23 @@ import {AiFillLinkedin} from 'react-icons/ai'
 import {AiOutlineInstagram} from 'react-icons/ai'
 import {BsTelephoneOutboundFill} from 'react-icons/bs'
 import {IoIosMail} from 'react-icons/io'
+import {AiFillInfoCircle} from 'react-icons/ai'
+import {MdOutlineMiscellaneousServices} from 'react-icons/md'
 
 const Footer = () => {
     return (
 
         <div className="container-fluid p-4 bg-light text-dark footer-container">
         <div className="logo-container">
-        <img src="https://res.cloudinary.com/dlovqnrza/image/upload/v1691648176/site-logo_vhtzvm.png" className = "footer-logo" alt="logo"/>
+       <Link href="/"> <img src="https://res.cloudinary.com/dlovqnrza/image/upload/v1691648176/site-logo_vhtzvm.png" className = "footer-logo" alt="logo"/></Link>
 
         </div>
         <div className="quick-links-container">
         <h1 className="footer-titles">QUICK LINKS</h1>
         <Link href="/"> <p className="footer-des"> <AiFillHome className="footer-icons"/> Home</p></Link>
-       <Link href="/about"> <p className="footer-des"> <FcAbout className="footer-icons"/>About Us</p></Link>
+       <Link href="/about"> <p className="footer-des"> <AiFillInfoCircle className="footer-icons"/>About Us</p></Link>
         <Link href="/contact"> <p  className="footer-des"><BsPhone className="footer-icons"/>Contact Us</p></Link>
-        <Link href="/services" ><p className="footer-des"><GrServices className="footer-icons"/>Services Us</p></Link>
+        <Link href="/services" ><p className="footer-des"><MdOutlineMiscellaneousServices className="footer-icons"/>Services Us</p></Link>
         </div>
         <div className="contact-us-links">
         <h1 className="footer-titles">SOCIAL NETWORKS</h1>
@@ -35,8 +37,8 @@ const Footer = () => {
         </div>
         <div className="contact-us-links">
         <h1 className="footer-titles">CONTACT US </h1>
-       <a tel="+44-7867090363" target="_blank"> <p className="footer-des"><BsTelephoneOutboundFill/> +44-7867090363</p></a>
-       <a href="info@prabisha.com" target="_blank"> <p className="footer-des"><IoIosMail/> info@prabisha.com</p></a>
+       <a  href="tel:+1234567890" target="_blank"> <p className="footer-des"><BsTelephoneOutboundFill/> +44-7867090363</p></a>
+       <a href="mailto:example@example.com" target="_blank"> <p className="footer-des"><IoIosMail/> info@prabisha.com</p></a>
 
         
         </div>
