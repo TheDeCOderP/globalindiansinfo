e548a10c044dacb40503bd7a6a720c0111e66f81
TheDeCOderP <113170140+TheDeCOderP@users.noreply.github.com>
2023-08-24 13:20:48 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@
 
 const nextConfig = {
 
+  // next.config.js
+module.exports = {
+  // Your other configuration options here
+  
+  // Example of a build-related configuration
+  distDir: 'build', // Customize the build output directory
+};
+
+
   };
   
   module.exports = nextConfig;
