7917c17271d9479aeecb54650d5c6de93e0ba3d4
prakash <developerprakash@gmail.com>
2023-08-30 11:19:38 +0530

    commit

--- api/blogs/blogs.js
+++ api/blogs/blogs.js
@@ -2,26 +2,34 @@ const Blogs = [
 
     {
         'id' : '1',
-        'title':'blog No 1',
-        'description':'blog description is here',
+        'title':'Navigating Cross-Cultural Identity: Insights from Global Indians',
+        'description':`<p><b>Navigating Cross-Cultural Identity: Insights from Global Indians</b></p>
+        <p>In an increasingly interconnected world, the concept of identity hasbecome more complex, especially for those who belong to multiple cultures. One such group is the Global Indian community, individuals of Indian origin who have settled in various parts of the world. Navigating a cross-cultural identity is a fascinating journey that involves balancing heritage, adapting to new environments, and forging a unique sense of self. In this blog, we delve into the experiences of Global Indians and uncover valuable insights into how they navigate their multifaceted identities.</p>
+        `,
         'imagepath':'uploads/images/blogs/blog1.jpg',
-        'slug':'blog1'
         
     },
     {
         'id' : '2',
-        'title':'blog No 2',
-        'description':'blog description is here',
+        'title':'Global Indians Unite: Stories of Connection and Belonging',
+        'description':`<p><b>Global Indians Unite: Stories of Connection and Belonging</b></p>
+        <p>In a world that's increasingly interconnected, the stories of Global Indians stand as inspiring narratives of connection, adaptation, and a profound sense of belonging. As individuals of Indian origin who have dispersed across the globe, they bring with them a unique blend of heritage and experiences. In this blog, we delve into the stories of Global Indians, exploring their journeys of building connections and finding a sense of belonging in diverse corners of the world.</p> `,
         'imagepath':'uploads/images/blogs/blog1.jpg',
-        'slug':'blog2'
+        
     },
     {
         'id' : '3',
         'title':'blog No 3',
         'description':'blog description is here',
         'imagepath':'uploads/images/blogs/blog1.jpg',
-        'slug':'blog3'
+        
     },
 ]
+
+
+// Generate slugs and assign them to the articles
+Blogs.forEach(blog => {
+    blog.slug = `${blog.title.toLowerCase().replace(/\s+/g, '-')}`;
+  });
 
 export default Blogs;
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,4 +1,4 @@
-// import Blogs from '@/api/blogs/blogs.js';
+ import Blogs from '@/api/blogs/blogs.js';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -7,17 +7,16 @@ const AllBlogs = () => {
      <div className="container">
         <div className="row">
 
-            <h2>Will Be Uploaded Soon... </h2>
-        {/* {
+         {
             Blogs.map((item) =>
             <div className="col-sm-12 col-lg-4 col-md-4" key={item.id}>
 
                 <Link href={`/blogs/${item.slug}`}>
                 <div className="blog_image">
-                    <Image src={`/${item.imagepath}`} width={300} height={300} alt="blogs"></Image>
+                    <Image src={`/${item.imagepath}`} width={300} height={300} alt={`blog$(item.id)`} className="aspect-ratio"></Image>
                 </div>
                 <div className="blog_body">
-                    <h4>{item.title}</h4>
+                    <h3 className="text-center text-dark">{item.title}</h3>
                 </div>
                 </Link>
 
@@ -25,7 +24,7 @@ const AllBlogs = () => {
             
             )
 
-        } */}
+        } 
         </div>
         </div>
     )
