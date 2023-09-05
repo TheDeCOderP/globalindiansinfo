ab3bec6bbcc23166d0e8051a5c0b17e12994a636
prakash <developerprakash@gmail.com>
2023-09-06 03:08:01 +0530

    cc

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const globalConfig = {
- /*   port: "https://server.globalindiansinfo.com", */
-  port: "http://localhost:5001",
+   port: "https://server.globalindiansinfo.com",
+ /*  port: "http://localhost:5001", */
   };
   
   export default globalConfig;
