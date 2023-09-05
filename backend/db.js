930957ada083172871b0cf241fe035841ef9bf40
prakash <developerprakash@gmail.com>
2023-09-06 02:44:54 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
   host: 'localhost',
-  user: 'prabisha', // Replace with your MySQL username
-  password: 'Prabisha@2024!', // Replace with your MySQL password
+  user: 'root', // Replace with your MySQL username
+  password: '', // Replace with your MySQL password
   database: 'prabisha_globalindiansinfo', // Replace with your MySQL database name
 });
 
