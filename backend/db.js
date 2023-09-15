1821d6cb8a8d5546a5e471308951285be7963380
prakash <developerprakash@gmail.com>
2023-09-15 17:48:57 +0530

    cd

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'consulting.prabisha.com',
+  host: 'localhost',
   user: 'prabisha', // Replace with your MySQL username
   password: 'Prabisha@2024!', // Replace with your MySQL password
   database: 'prabisha_globalindiansinfo', // Replace with your MySQL database name
