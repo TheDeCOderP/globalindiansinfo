e624d45614d3067fe77cc4115a14c64652aa2a78
prakash <developerprakash@gmail.com>
2023-09-20 22:41:21 +0530

    commit

--- components/pages/articles/categories/news.js
+++ components/pages/articles/categories/news.js
@@ -26,17 +26,20 @@ const  EventsArticles = () =>{
   return (
     <div className="row mt-2">
     <h1 className="text-center"><span className="green">Latest</span> News</h1>
-     <div class="book col-sm-12 col-md-4 col-lg-4 mb-4 pb-2 ">
-        <img class="book-image" src="/uploads/images/news/book.jpg" alt="Image Alt Text" width={400} height={300}  />
-        <h2 class="book-title">Bharat- Mother of Democracy</h2>
-        <a class="book-button text-dark bg-light" href="https://ebook.g20.org/ebook/bharatmod/index.html" target="_blank" rel="noopener noreferrer">Read More</a>
+     <div className="book col-sm-12 col-md-4 col-lg-4 mb-4 pb-2 ">
+        <img className="book-image aspect-ratio" src="/uploads/images/news/book.jpg" alt="Image Alt Text"   />
+        <div className="books_box">
+        <h2 className="book-title">Bharat- Mother of Democracy</h2>
+        <a className="book-button text-dark bg-light" href="https://ebook.g20.org/ebook/bharatmod/index.html" target="_blank" rel="noopener noreferrer">Read More</a>
     </div>
-    <div class="book col-sm-12 col-md-4 col-lg-4  mb-4 pb-2">
-        <img class="book-image" src="/uploads/images/jobs/uk.jpg" alt="Image Alt Text" width={400} height={300}  />
-        <h2 class="book-title">Big steps forward in capital markets  
+    </div>
+    <div className="book col-sm-12 col-md-4 col-lg-4  mb-4 pb-2">
+        <img className="book-image aspect-ratio" src="/uploads/images/jobs/uk.jpg" alt="Image Alt Text" />
+        <div className="books_box">
+        <h2 className="book-title">Big steps forward in capital markets  
 </h2>
-        <a class="book-button text-dark bg-light" href="https://hmtreasury-newsroom.prgloo.com/news/big-steps-forward-in-capital-markets-cooperation-with-india" target="_blank" rel="noopener noreferrer">Read More</a>
-    </div>
+        <a className="book-button text-dark bg-light" href="https://hmtreasury-newsroom.prgloo.com/news/big-steps-forward-in-capital-markets-cooperation-with-india" target="_blank" rel="noopener noreferrer">Read More</a>
+    </div></div>
   
   
       <div className="row mt-5">
