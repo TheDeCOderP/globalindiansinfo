9060d08319279a26a0a8ee61f3ded8da57bbef61
prakash <developerprakash@gmail.com>
2023-09-26 10:35:23 +0530

    Prakash

--- components/layout/header/mobilenav.js
+++ components/layout/header/mobilenav.js
@@ -12,7 +12,8 @@ import {
 } from "@chakra-ui/react";
 import { Modal , ModalOverlay } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import Menus from "@/api/menus/menus";
+import Menuitems from "@/api/menus/menus";
+import Dropdown from 'react-bootstrap/Dropdown';
 import Link from "next/link";
 import Image from "next/image";
 
@@ -78,18 +79,24 @@ const MobileMenu = () => {
             </Link>
           </DrawerHeader>
           <DrawerBody>
-            {Menus.map((item) => (
-              <div className="mobile_menu" key={item.id}>
-                <Link
-                  className="text-dark"
-                  href={`${item.slug}`}
-                  as={`${item.slug}`}
-                  onClick={onClose}
-                >
-                  {item.name}
-                </Link>
-              </div>
-            ))}
+          {Menuitems.map((ele) =>
+            ele.submenu ? (
+              <Dropdown key={ele.id} className="mobile_menu" >
+                <Dropdown.Toggle id={`dropdown-${ele.id}`} className='' >
+                 <Link     key={ele.id} className='mobile_links'  href={ele.slug} as={`${ele.slug}`} >{ele.icon} <span>{ele.name}</span></Link>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  {ele.submenu.map((ele1) => (
+                    <Dropdown.Item key={ele1.id}>
+                      <Link  onClick={()=> ele1.name=='Upskill' && console.log(ele1.slug) }  className='links' href={ele1.slug} as={`${ele1.slug}`}  >{ele1.icon} <span className="button-content">{ele1.name}</span></Link>
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+            ) : (
+              <Link   key={ele.id} className={ele.name=="Apply For Jobs" || ele.name=="Hire Talent" || ele.name=="MagicA10"?'navlinks':'links'} href={ele.slug} as={`${ele.slug}`} >{ele.icon}<span className="button-content">{ele.name} </span></Link>
+            )
+          )}
           </DrawerBody>
         </DrawerContent>
       </Drawer>
