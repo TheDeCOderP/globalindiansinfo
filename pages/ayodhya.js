461ee02ee962dfc8f43a13655446612944954b1d
prakash <developerprakash@gmail.com>
2024-01-19 23:04:47 +0530

    PP

--- pages/ayodhya.js
+++ pages/ayodhya.js
@@ -29,57 +29,100 @@ const PdfPreview = () => {
     }
   };
 
-  
-  
+  const handleDownloadPDF2 = async () => {
+    try {
+      const response = await fetch('/pdf/Ayodhya-2024.pdf');
+      const blob = await response.blob();
+
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = 'Ayodhya-2024.pdf';
+      document.body.appendChild(link);
+
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error('Error downloading PDF:', error);
+    }
+  };
+
+
+
 
   pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
   return (
     <div className="container mt-5 mb-5">
       <Head>
-      <title>Ayodhya - Global Indians Info</title>
+        <title>Ayodhya - Global Indians Info</title>
       </Head>
       <div className="row flex justify-content-center">
-       
-        <div className="col-md-12 col-sm-12 ">
-        <div className='m-2  p-3 pt-4 box-shadow'>
-        <div className="text-center ">
-          <h1 className='text-center mb-5'>10 Leadership Lessons from Prabhu Shri Ram</h1>
-        <Link href="/pdf/10-Leaders-Lesson-from-Prabhu-Shri-Ram.pdf" target="_blank" className="m-3"> <button
-              className="btn btn-success p-2"
-             
-            >
-              View PDF
-            </button>
-            </Link>
-            <button
-              className="button rounded mb-3"
-              onClick={handleDownloadPDF1}
-            >
-              Download PDF
-            </button>
+        <h1 className='text-center mb-2'>10 Leadership Lessons from Prabhu Shri Ram</h1>
+        <div className="col-md-6 col-sm-12 ">
+          <div className='m-2  p-3 pt-4 box-shadow'>
+            <div className="text-center ">
 
-          
+              <Link href="/pdf/10-Leaders-Lesson-from-Prabhu-Shri-Ram.pdf" target="_blank" className="m-3"> <button
+                className="btn btn-success p-2"
+
+              >
+                View PDF
+              </button>
+              </Link>
+              <button
+                className="button rounded mb-3"
+                onClick={handleDownloadPDF1}
+              >
+                Download PDF
+              </button>
+
+
+            </div>
+
+
+
+            <div className="d-flex justify-content-center mb-4">
+
+            <Link href="/pdf/10-Leaders-Lesson-from-Prabhu-Shri-Ram.pdf" target="_blank" className="m-3">
+                <img src="/pdf/ayodhya-1.jpg" className='img-fluid'></img>
+              </Link>
+            </div>
+
           </div>
-          
-          
-
-          <div className="d-flex justify-content-center mb-4">
-            
-  <div className="embed-responsive embed-responsive-16by9">
-    <iframe
-      className="embed-responsive-item"
-      src="/pdf/10-Leaders-Lesson-from-Prabhu-Shri-Ram.pdf"
-      title="PDF Preview"
-      allowFullScreen
-      style={{ width: '100%', maxWidth: '700px', height: '400px' }}
-    ></iframe>
-  </div>
-</div>
-
         </div>
-      </div>
-    
+        <div className="col-md-6 col-sm-12 ">
+          <div className='m-2  p-3 pt-4 box-shadow'>
+            <div className="text-center ">
+
+              <Link href="/pdf/Ayodhya-2024.pdf" target="_blank" className="m-3"> <button
+                className="btn btn-success p-2"
+
+              >
+                View PDF
+              </button>
+              </Link>
+              <button
+                className="button rounded mb-3"
+                onClick={handleDownloadPDF2}
+              >
+                Download PDF
+              </button>
+
+
+            </div>
+
+
+
+            <div className="d-flex justify-content-center mb-4">
+
+            <Link href="/pdf/Ayodhya-2024.pdf" target="_blank" className="m-3">
+                <img src="/pdf/ayodhya-2.jpg" className='img-fluid'></img>
+              </Link>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     </div>
   );
